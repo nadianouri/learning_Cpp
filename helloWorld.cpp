c088dd42d8721aa49414dac6441e8c4fff43a88f
Nadia Nouri <nadianour169@gmail.com>
2023-03-28 00:21:22 +0200

    data types and variables

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    std ::cout << "Hello worldd" << std::endl;
+    cout << "Nadia Nouri\n"; // \n stands for new line
+    cout << "This is my first Cpp program" << endl; //end line /new line
+    cout << "The result of "<< "5 + 10 = " << 5+10 << endl << endl;
+    cout << "*       * \n";
+    cout << "*       * \n";
+    cout << "* * * * * \n";
+    cout << "*       * \n";
+    cout << "*       * \n\n";
+    cout << "*************************************************\n";
+    cout << "Name: Nadia Nouri.\n";
+    cout << "Age: 32 years.\n";
+    cout << "City: Stockholm.\n";
+    cout << "Country: Sweden.\n";
+    cout << "**************************************************\n\n";
+    cout << "**************\n";
+    cout << "**************\n";
+    cout << "**************\n";
+    cout << "**************\n\n";
+    cout << "I love programming!\n\n";
+    cout << "I promise to be the best developer ever.\n\n";
+    cout << "I know it will take some time to practice,but I will achieve my goal.\n\n";
+    cout << "Best Regards,\n";
+    cout << "Nadia Nouri.\n";
+    cout << "M1\\M2\n\n";
+    //cout << "\a" <<endl;
+    cout << "Dear sir \\ Madam,\n\n";
+    cout << "How are you?\n\n";
+    cout << "My name is \"Nadia\",nice to meet you.\n\n";
+    cout << "Ali \t Ahmed \t Lina"<< endl;
+    cout << "Fadi \t Zain \t Mona"<< endl;
+
+
+
     return 0;
-}
+}   
---
+++ homeWork.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+//homewrok 1
+string fullName = "Nadia Nouri";
+
+int age = 32;
+string city = "Stockholm";
+string country = "Sweeden";
+int monthlySalary = 5000;
+char gender = 'M';
+bool isMarried = false;
+
+int main(){
+    cout << "Name: "<< fullName << ".\n";
+    cout << "Age: "<< age << " Years.\n";
+    cout << "City: "<< city << ".\n";
+    cout << "Country: "<< country << ".\n";
+    cout << "Monthly Salary: "<< monthlySalary <<".\n";
+    cout << "Yearly Salary: "<< monthlySalary * 12 << ".\n";
+    cout << "Gender: "<< gender << ".\n";
+    cout << "Married: "<< isMarried << ".\n\n";
+    //homework 2
+    int num_1 = 20 , num_2 = 30 ,num_3 = 10;
+    cout << num_1 << "+" <<endl;
+    cout << num_2 << "+" <<endl;
+    cout << num_3 << endl;
+    cout << "________________________"<<endl<<endl;
+    cout << "Total = " << num_1 + num_2 + num_3 <<endl;
+    //homework 3
+    int my_age = 25;
+    int yourAgeAfterFiveYears = my_age + 5;
+    cout <<"After 5 years you will be "<< yourAgeAfterFiveYears <<" years old. \n";
+     
+
+
+
+
+
+}
+
+ 
